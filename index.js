e3d0218226980feeb437ffe5edd5567e298e52cf
Dylson Valente Neto <ama@n370.info>
2018-02-22 12:12:41 -0300

    Use console.log

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
 module.exports = (req) => {
     return req
-    .on('error', warn.bind(null, 'REQUEST ERROR EVENT'))
-    .on('abort', warn.bind(null, 'REQUEST ABORT EVENT'))
-    .on('aborted', warn.bind(null, 'REQUEST ABORTED EVENT'))
-    .on('connect', warn.bind(null, 'REQUEST CONNECT EVENT'))
-    .on('continue', warn.bind(null, 'REQUEST CONTINUE EVENT'))
-    .on('upgrade', warn.bind(null, 'REQUEST UPGRADE EVENT'))
-    .on('socket', socket => {
-        warn('REQUEST SOCKET EVENT')
-        socket
-        .on('close', warn.bind(null, 'REQUEST SOCKET CLOSE EVENT'))
-        .on('connect', warn.bind(null, 'REQUEST SOCKET CONNECT EVENT'))
-        .on('data', warn.bind(null, 'REQUEST SOCKET DATA EVENT'))
-        .on('drain', warn.bind(null, 'REQUEST SOCKET DRAIN EVENT'))
-        .on('end', warn.bind(null, 'REQUEST SOCKET END EVENT'))
-        .on('error', error.bind(null, 'REQUEST SOCKET ERROR EVENT'))
-        .on('lookup', warn.bind(null, 'REQUEST SOCKET LOOKUP EVENT'))
-        .on('timeout', warn.bind(null, 'REQUEST SOCKET TIMEOUT EVENT'))
-    })
-    .on('response', response => {
-        warn('REQUEST RESPONSE EVENT')
-        response
-        .on('aborted', error.bind(null, 'RESPONSE ABORTED EVENT'))
-        .on('close', error.bind(null, 'RESPONSE CLOSE EVENT'))
+        .on('error', log.bind(null, 'REQUEST ERROR EVENT'))
+        .on('abort', log.bind(null, 'REQUEST ABORT EVENT'))
+        .on('aborted', log.bind(null, 'REQUEST ABORTED EVENT'))
+        .on('connect', log.bind(null, 'REQUEST CONNECT EVENT'))
+        .on('continue', log.bind(null, 'REQUEST CONTINUE EVENT'))
+        .on('upgrade', log.bind(null, 'REQUEST UPGRADE EVENT'))
+        .on('socket', socket => {
+            log('REQUEST SOCKET EVENT')
+            socket
+                .on('close', log.bind(null, 'REQUEST SOCKET CLOSE EVENT'))
+                .on('connect', log.bind(null, 'REQUEST SOCKET CONNECT EVENT'))
+                .on('data', log.bind(null, 'REQUEST SOCKET DATA EVENT'))
+                .on('drain', log.bind(null, 'REQUEST SOCKET DRAIN EVENT'))
+                .on('end', log.bind(null, 'REQUEST SOCKET END EVENT'))
+                .on('error', log.bind(null, 'REQUEST SOCKET ERROR EVENT'))
+                .on('lookup', log.bind(null, 'REQUEST SOCKET LOOKUP EVENT'))
+                .on('timeout', log.bind(null, 'REQUEST SOCKET TIMEOUT EVENT'))
+        })
+        .on('response', response => {
+            log('REQUEST RESPONSE EVENT')
+            response
+                .on('aborted', log.bind(null, 'RESPONSE ABORTED EVENT'))
+                .on('close', log.bind(null, 'RESPONSE CLOSE EVENT'))
 
-        response.socket
-        .on('close', error.bind(null, 'RESPONSE SOCKET CLOSE EVENT'))
-        .on('connect', error.bind(null, 'RESPONSE SOCKET CONNECT EVENT'))
-        .on('data', error.bind(null, 'RESPONSE SOCKET DATA EVENT'))
-        .on('drain', error.bind(null, 'RESPONSE SOCKET DRAIN EVENT'))
-        .on('end', error.bind(null, 'RESPONSE SOCKET END EVENT'))
-        .on('error', error.bind(null, 'RESPONSE SOCKET ERROR EVENT'))
-        .on('lookup', warn.bind(null, 'RESPONSE SOCKET LOOKUP EVENT'))
-        .on('timeout', warn.bind(null, 'RESPONSE SOCKET TIMEOUT EVENT'))
+            response.socket
+                .on('close', log.bind(null, 'RESPONSE SOCKET CLOSE EVENT'))
+                .on('connect', log.bind(null, 'RESPONSE SOCKET CONNECT EVENT'))
+                .on('data', log.bind(null, 'RESPONSE SOCKET DATA EVENT'))
+                .on('drain', log.bind(null, 'RESPONSE SOCKET DRAIN EVENT'))
+                .on('end', log.bind(null, 'RESPONSE SOCKET END EVENT'))
+                .on('error', log.bind(null, 'RESPONSE SOCKET ERROR EVENT'))
+                .on('lookup', log.bind(null, 'RESPONSE SOCKET LOOKUP EVENT'))
+                .on('timeout', log.bind(null, 'RESPONSE SOCKET TIMEOUT EVENT'))
     });
 };
 
-function warn(message) {
-    console.warn(message);
-}
-
-function error(message) {
-    console.error(message);
+function log (message) {
+    console.log(message);
 }
